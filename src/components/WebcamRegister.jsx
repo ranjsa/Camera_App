a97e1fad34a615d5dd7141a6d838e12792a896cd
Tushar <gauravtushar7@gmail.com>
2021-07-18 15:40:59 +0530

    latest chnages till 3:40

--- src/components/WebcamRegister.jsx
+++ src/components/WebcamRegister.jsx
@@ -9,29 +9,29 @@ const videoConstraints = {
 };
 
 const WebcamRegister = () => {
-  const imgArray = [];
+  const [imgArr, setImgArr] = useState([]);
   const webcamRef = React.useRef(null);
   const [count, setCount] = useState(5);
   const [makeSubmmit, setSubmit] = useState(false);
   const capture = () => {
     if (count >= 1) {
       setCount(count - 1);
-      const imageSrc = webcamRef.current.getScreenshot();
-      imgArray.push(imageSrc);
-      console.log(imgArray);
-      console.log(count);
+      let imageSrc = webcamRef.current.getScreenshot();
+      imageSrc = imageSrc.replace("data:image/jpeg;base64,","");
+      setImgArr([... imgArr, {id: imgArr.length, value: imageSrc}])
     } else {
       setSubmit(true);
     }
   };
   const userKYC = {
     customer_id: 'A37B2O7S12FMWC',
-    image_array: imgArray,
+    image: imgArr
   };
   const formSubmit = async () => {
+    // await setImageArr(JSON.stringify(imgArray));
     try {
       const response = await axios.post(
-        'https://uolkz55l63.execute-api.us-east-1.amazonaws.com/v2/upload',
+        'https://w7hfmjr0b8.execute-api.us-east-1.amazonaws.com/v1/register',
         { userKYC },
         {
           headers: {
@@ -49,13 +49,13 @@ const WebcamRegister = () => {
   };
   return (
     <>
-      <h1 style={{backgroundColor: "#000000"}} className="text-center py-5 text-xl font-bold text-white">
+      <h1 style={{backgroundColor: "#000000"}} className="text-center font-sans py-5 text-xl font-bold text-white">
         Capture and Submit Your Image!
       </h1>
       <div className="">
         <div className="my-10">
           <div class="flex flex-wrap justify-center">
-            <div class="w-6/12 sm:w-4/12 px-4">
+            <div class="w-6/12 sm:w-4/12 px-4 shadow-2xl">
               <Webcam
                 audio={false}
                 height={400}
@@ -76,7 +76,7 @@ const WebcamRegister = () => {
                 e.preventDefault();
                 formSubmit();
               }}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
             >
               Submit
             </button>
@@ -87,10 +87,10 @@ const WebcamRegister = () => {
                 e.preventDefault();
                 capture();
               }}
-              className="text-white font-bold py-2 px-4 rounded"
+              className="text-white font-bold py-2 px-4"
             >
-              Capture
-              {' ' + count}
+              <h1 className="font-bold">Capture {' ' + count}</h1>
+              
             </button>
           )}
         </div>
